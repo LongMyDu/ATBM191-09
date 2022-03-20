038d43b64ef782387e8326ba711729b9efa2e62f
LongMyDu <longmydu@gmail.com>
2022-03-20 22:20:05 +0700

    Try connect to Oracle

--- ATBM191-09-UI/Form1.cs
+++ ATBM191-09-UI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
 
 namespace ATBM191_09_UI
 {
@@ -15,6 +16,33 @@ namespace ATBM191_09_UI
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                string ConString = "Data Source=XEPDB1;User Id=QLHOCPHAN_ADMIN;Password=qlhocphanadmin;";
+                using (OracleConnection con = new OracleConnection(ConString))
+                {
+                    OracleCommand cmd = new OracleCommand("SELECT * FROM QLHOCPHAN_ADMIN.VW_GVU_SINHVIEN", con);
+                    OracleDataAdapter oda = new OracleDataAdapter(cmd);
+
+                    DataSet ds = new DataSet();
+
+                    oda.Fill(ds);
+                    if (ds.Tables.Count > 0)
+                    {
+                        dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }

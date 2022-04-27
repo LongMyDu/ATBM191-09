d4e12dda5c3be61e038c99bafe7d0431b639c684
MinhTran01 <tranthihueminh14112001@gmail.com>
2022-04-27 13:49:56 +0700

    Update CREATE-TABLE

--- CREATE-TABLE.sql
+++ CREATE-TABLE.sql
@@ -1,5 +1,5 @@
 /*
-ƒê·ªí √ÅN M√îN H·ªåC AN TO√ÄN B·∫¢O M·∫¨T D·ªÆ LI·ªÜU TRONG H·ªÜ TH·ªêNG TH√îNG TIN
+ƒ?·ªí √?N M√îN H·ªåC AN TO√ÄN B·∫¢O M·∫¨T D·ªÆ LI·ªÜU TRONG H·ªÜ TH·ª?NG TH√îNG TIN
 FILE T·∫†O DATABASE 
 NH√ìM 9
 
@@ -107,110 +107,21 @@ ADD CONSTRAINT NHANVIEN_VAITRO_FK
 	CHECK (VAITRO IN (N'Thanh tra', N'C∆° s·ªü y t·∫ø', N'Y sƒ©/b√°c sƒ©', N'Nghi√™n c·ª©u'));
 
 
-create view vw_hsban as select * from hsban;
-create view vw_hsban_dv as select * from hsban_dv;
+
+-- INSERT DATA TO NHANVIEN
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0001',N'B?nh vi?n ?a khoa V?n H?nh',N'700 S? V?n H?nh (n?i d‡i) - P 12 ñ Qu?n 10 - TP.HCM', '0986875637');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0002',N'B?nh vi?n T‚n S?n Nh?t',N'2B Ph? Quang - P2 - Qu?n T‚n BÏnh - TP.HCM', '0902318877');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0003',N'B?nh vi?n ?a khoa T‚n H?ng ',N'871 Tr?n Xu‚n So?n - P. T‚n H?ng - Qu?n 7 - TP.HCM', '0913777399');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0004',N'B?nh vi?n Th?ng Nh?t',N'01 L˝ Th??ng Ki?t ñ Ph??ng 7 - Qu?n T‚n BÏnh - TP.HCM', '0966821010');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0005',N'B?nh vi?n 175',N'786 Nguy?n Ki?m- P. 3- Qu?n GÚ v?p - TP.HCM', '0984795351');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0006',N'B?nh vi?n Nguy?n Tri Ph??ng',N'468 Nguy?n Tr„i ñ Ph??ng 8 ñ Qu?n 5 - TP.HCM', '0931427504');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0007',N'B?nh vi?n Nh‚n d‚n Gia ??nh',N'01 N? Trang Long ñ Ph??ng 7 ñ Qu?n BÏnh Th?nh - TP.HCM', '0902418869');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0008',N'B?nh vi?n nh‚n d‚n 115',N'527 S? V?n H?nh ñ Ph??ng 12 ñ Qu?n 10 - TP.HCM', '0907527643');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0009',N'B?nh vi?n ?a khoa S‡i GÚn',N'125 LÍ L?i ñ Ph??ng B?n Th‡nh ñ Qu?n 1 - TP.HCM', '0902963115');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS0010',N'B?nh vi?n Qu‚n D‚n Mi?n ?Ùng ',N'50 LÍ V?n Vi?t ñ Ph??ng Hi?p Ph˙ - Qu?n 9 - TP.HCM', '0908376664');
+
+SELECT * FROM CSYT
 
 
-create table nhanvien(manv number, hoten varchar2(50));
-
-insert into nhanvien values (1, 'My Du');
-insert into nhanvien values (2, 'Khanh Duy');
-insert into nhanvien values (3, 'Minh');
-insert into nhanvien values (4, 'Nhi');
-
-select * from nhanvien;
 
 
-SELECT
-*
-FROM
- NHANVIEN   ;
- 
- commit;
- 
- 
- SELECT * FROM USER_VIEWS
-
-select * from user_views;
-
-select column_id, 
-       owner as schema_name,
-       table_name, 
-       column_name, 
-       data_type, 
-       data_length, 
-       data_precision, 
-       data_scale, 
-       nullable
-from sys.all_tab_columns
-where owner = (select user from dual)
-order by table_name;
-
-select decode( col.table_name
-, lag(col.table_name, 1) over(order by col.table_name)
-, null
-, col.table_name ) view_name,
-col.column_name, 
-col.data_type, 
-col.data_length, 
-col.data_precision, 
-col.data_scale, 
-col.nullable
-from sys.all_tab_columns col
-inner join sys.all_views v on col.owner = (select user from dual)
-and col.table_name = v.view_name
-order by col.table_name;
-
-select decode( col.table_name
-, lag(col.table_name, 1) over(order by col.table_name)
-, null
-, col.table_name ) view_name,
-col.column_name, 
-col.data_type, 
-col.data_length, 
-col.data_precision, 
-col.data_scale, 
-col.nullable
-from sys.all_tab_columns col
-inner join sys.all_tables v on col.owner = (select user from dual)
-and col.table_name = v.table_name
-order by col.table_name;
-select * from sys.all_tables sat where sat.owner = (select user from dual);
-
-select decode(role, lag(role, 1) over(order by role), null, role) role, privilege, admin_option, common, inherited from role_sys_privs order by role;
-select * from dba_roles;
-grant create any table to role_giaovu;
- select *
-from role_sys_privs rsp
-join dba_roles  dr on rsp.role = dr.role
-order by rsp.role;
-
-select * from User_col_privs where grantee='AAAAAA';
-
-select granted_role, 'YES' is_granted from dba_role_privs where grantee='GIAOVU1';
-(select granted_role, 'YES' is_granted, admin_option from dba_role_privs where grantee='GIAOVU1') union (select role as granted_role, 'NO' is_granted, 'NO' admin_option from dba_roles where role not in (select granted_role from dba_role_privs where grantee='GIAOVU1'));
-
-
-select ar.role, gr.admin_option, gr.grantee
-from dba_roles ar LEFT JOIN dba_role_privs gr on (ar.role = gr.granted_role and gr.grantee='GIAOVU1')
-order by ar.role;
-
-
-select distinct table_name from user_tab_columns;
-
-select GRANTEE AS "USER", TABLE_NAME, "PRIVILEGE", GRANTABLE, GRANTOR from dba_tab_privs where GRANTEE ='AAAAAA';
-select GRANTEE AS "USER", TABLE_NAME, COLUMN_NAME, "PRIVILEGE", GRANTABLE, GRANTOR from User_col_privs where GRANTEE = 'AAAAAA';
-
-
-grant select, insert, update, delete on benhnhan to AAAAAA;
-revoke update on benhnhan from AAAAAA;
-
-grant update(macsyt, tiensubenh, tiensubenhgd) on benhnhan to AAAAAA;
-
-
-select GRANTEE AS \"USER\", TABLE_NAME, \"PRIVILEGE\", GRANTABLE, GRANTOR from dba_tab_privs where GRANTEE ='{userProperties.username}';
-
-select COLUMN_NAME, "PRIVILEGE", GRANTABLE from User_col_privs 
-where table_name = 'BENHNHAN' and grantee = 'AAAAAA' and PRIVILEGE='UPDATE';
-
-grant select on VW_HSBAN to ROLE_GIAOVIEN with grant option;

b19b88aded2fc842f1936d9dce31cefe681a6e62
LongMyDu <longmydu@gmail.com>
2022-04-27 23:14:48 +0700

    Cài đặt chính sách 6

--- NHAP.sql
+++ NHAP.sql
@@ -1,3 +1,74 @@
+select 'Drop user "'||CMND||'" cascade;' from NHANVIEN;
+
+Drop user "916773394945" cascade;
+Drop user "477197069125" cascade;
+Drop user "644198063807" cascade;
+Drop user "307780507980" cascade;
+Drop user "480148058972" cascade;
+Drop user "625421696262" cascade;
+Drop user "093142227246" cascade;
+Drop user "908519739639" cascade;
+Drop user "611825513067" cascade;
+Drop user "828559055899" cascade;
+
+
+Drop user "410963698240" cascade;
+Drop user "917138329545" cascade;
+Drop user "829252737656" cascade;
+Drop user "758234874761" cascade;
+Drop user "084456993664" cascade;
+Drop user "675756075261" cascade;
+Drop user "859242462428" cascade;
+Drop user "486589763960" cascade;
+Drop user "503192156214" cascade;
+Drop user "770179263005" cascade;
+Drop user "626511554232" cascade;
+Drop user "088272820825" cascade;
+Drop user "651823963095" cascade;
+Drop user "705720922190" cascade;
+Drop user "287673062535" cascade;
+Drop user "313847922476" cascade;
+Drop user "196893690493" cascade;
+Drop user "324913323649" cascade;
+Drop user "792452447502" cascade;
+Drop user "803019783337" cascade;
+Drop user "031900361300" cascade;
+Drop user "648537445435" cascade;
+Drop user "997835560317" cascade;
+Drop user "931361921026" cascade;
+Drop user "580295250641" cascade;
+Drop user "865766193037" cascade;
+Drop user "478136075046" cascade;
+Drop user "799272390265" cascade;
+Drop user "740214271755" cascade;
+Drop user "557610679443" cascade;
+Drop user "814924308375" cascade;
+Drop user "734664302041" cascade;
+Drop user "427628358443" cascade;
+Drop user "459171471634" cascade;
+Drop user "453679954272" cascade;
+Drop user "399981059659" cascade;
+Drop user "108914194564" cascade;
+Drop user "435425344998" cascade;
+Drop user "203322515882" cascade;
+Drop user "747376189404" cascade;
+Drop user "041006808743" cascade;
+Drop user "529609359645" cascade;
+Drop user "076110272515" cascade;
+Drop user "465442648845" cascade;
+Drop user "341370236279" cascade;
+Drop user "491646195877" cascade;
+Drop user "831053317484" cascade;
+Drop user "669560958954" cascade;
+Drop user "192131394187" cascade;
+Drop user "081066600741" cascade;
+Drop user "149142094761" cascade;
+Drop user "246664081455" cascade;
+Drop user "486029082553" cascade;
+Drop user "434137157331" cascade;
+Drop user "139101531202" cascade;
+
+
 create view vw_hsban as select * from hsban;
 create view vw_hsban_dv as select * from hsban_dv;
 

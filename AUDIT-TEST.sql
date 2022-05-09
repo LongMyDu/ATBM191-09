01426c2d104fa9a40608ef9550edd45a8b7524d1
LongMyDu <longmydu@gmail.com>
2022-05-09 21:25:13 +0700

    audit

--- AUDIT-TEST.sql
+++ AUDIT-TEST.sql
@@ -1,13 +1,18 @@
+-- Test CS .1
 -- Thử xóa bệnh nhân BN000001
 Exec QLCSYTE_ADMIN.XoaBenhNhan('BN000001');
 
 -- Thử thêm bệnh nhân BN000001
 EXEC QLCSYTE_ADMIN.THEMBENHNHAN('BN000001','CS0002', N'Trần Việt Đào Bình','916773394945', '1954-03-11', '148', N'3 tháng 2', N'Q.1', N'Cần Thơ', N'NULL', N'NULL', N'NULL','TyHFH6hOI2Hz97BX');
 
+-- Test CS .2
 -- Thử thêm 1 dòng vào HSBA
 INSERT INTO HSBA(MAHSBA,MABN,NGAY,CHANDOAN,MABS,MAKHOA,MACSYT,KETLUAN) 
 VALUES ('HSBA0001', 'BN000001', TO_DATE('2001-01-01', 'yyyy-mm-dd'), 'chuan doan 1', null, 'K00001', 'CS0001', 'ket luan 1');
 
+-- Test CS .3
 DELETE FROM HSBA;
 
-select* from nhanvien;
+-- Test CS .4
+--Connect bang BACSI (hoặc bất kỳ user nào được select benhnhan), nếu select fga_trail có lưu lại là đúng
+select * from qlcsyte_admin.benhnhan;
--- AUDIT.sql
+++ AUDIT.sql
@@ -8,8 +8,10 @@
 --Connect bằng user pdb. Khuyến cáo: thao tác này xóa hết tất cả những gì đã từng audit từ trước tới giờ!!!
 delete from SYS.AUD$;
 commit; 
+GRANT EXECUTE ON DBMS_FGA TO QLCSYTE_ADMIN;
+
 --Từ khúc này có thể connect bằng QLCSYTE_ADMIN
---Xem thông tin đã được AUDIT:
+--Xem thông tin đã được standart AUDIT :
 select
    os_username,
    username,
@@ -20,18 +22,27 @@ select
    sql_bind,
    sql_text
 from
-   dba_audit_trail
+   DBA_AUDIT_TRAIL
 ORDER BY TIMESTAMP DESC;
 
+-- Xem thông tin đã được Fine grain audit
+select * from dba_fga_audit_trail;
 
--- Chính sách: Audit lại tất cả hành động exec các PROCEDURE sau: THEMBENHNHAN, XOABENHNHAN, THEMNHANVIEN, XOANHANVIEN
+-- (SA)Chính sách .1: Audit lại tất cả hành động exec các PROCEDURE sau: THEMBENHNHAN, XOABENHNHAN, THEMNHANVIEN, XOANHANVIEN
 AUDIT EXECUTE ON QLCSYTE_ADMIN.THEMBENHNHAN BY ACCESS;
 AUDIT EXECUTE ON QLCSYTE_ADMIN.XOABENHNHAN BY ACCESS;
 AUDIT EXECUTE ON QLCSYTE_ADMIN.THEMNHANVIEN BY ACCESS;
 AUDIT EXECUTE ON QLCSYTE_ADMIN.XOANHANVIEN BY ACCESS;
 
--- Chính sách: Audit lại tất cả hành động update, delete, insert trên bảng HSBA
+-- (SA)Chính sách .2: Audit lại tất cả hành động update, delete, insert trên bảng HSBA
 AUDIT UPDATE, DELETE, INSERT ON QLCSYTE_ADMIN.HSBA BY ACCESS;
 
--- Chính sách: Audit lại tất cả hành động update, delete, insert trên bảng HSBA_DV
+-- (SA)Chính sách .3: Audit lại tất cả hành động update, delete, insert trên bảng HSBA_DV
 AUDIT UPDATE, DELETE, INSERT ON QLCSYTE_ADMIN.HSBA_DV BY ACCESS;
+
+-- Để FGA, cần chỉnh lại cái này, audit trên bảng nào thì chỉnh lại trên bảng đó
+ANALYZE TABLE BENHNHAN COMPUTE STATISTICS;
+
+-- (FGA)Chính sách .4: Audit lại tất cả hành động select CMND trên bảng BENHNHAN của tất cả user mà không phải bệnh nhân đó
+EXEC DBMS_FGA.ADD_POLICY (object_schema => 'QLCSYTE_ADMIN', object_name => 'BENHNHAN', policy_name =>  'Select_CMND_BENHNHAN', audit_condition => 'MABN != USER', audit_column => 'CMND', statement_types => 'SELECT'); 
+
